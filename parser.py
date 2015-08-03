a4eb6f06bfc7c91ea41940782bff7eb08ab09bce
Guilherme Cardoso <gcsoares@inf.ufrgs.br>
2015-08-02 22:37:16 -0300

    Update parser.py

--- parser.py
+++ parser.py
@@ -14,24 +14,29 @@ class Parser(HTMLParser):
 				if(a[1] == "span1"):
 					print "},"
 					print "{"
-					sys.stdout.write("inscricao: ")
+					sys.stdout.write("\"inscricao\": ")
 				elif(a[1] == "span2"):
-					sys.stdout.write("nome: ")
+					sys.stdout.write("\"nome\": ")
 				elif(a[1] == "span3"):
-					sys.stdout.write("semestre: ")
+					sys.stdout.write("\"semestre\": ")
 				elif(a[1] == "span4"):
-					sys.stdout.write("curso: ")
+					sys.stdout.write("\"curso\": ")
 
 	def handle_data(self, data):
-		print data,","
+		if(data != '*' and data != "Há outro candidato com nome idêntico. Confirme pelo número de inscrição."):
+			sys.stdout.write("\"")
+			sys.stdout.write(data)
+			print("\",")
 
 def main():
 	url = sys.argv[1]
 	response = urllib2.urlopen(url)
 	page = response.read()
 	p = Parser()
+	print "["
 	p.feed(page)
-	print '}'
+	print "}"
+	print "]"
 
 if __name__ == "__main__":
 	main()
